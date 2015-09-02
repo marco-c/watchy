d2eaf3b67f7afb1683a85ace3f420254577a8391
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-03 01:25:31 +0200

    WIP - Search shows

---
+++ app/controllers/series-new.js
@@ -0,0 +1,73 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isSearching: false,
+
+  resultCount: 0,
+  results: null,
+
+  search: function(query) {
+      var _this = this;
+
+      if (this.get('isSearching')) {
+          return;
+      }
+
+      this.set('isSearching', true);
+
+      return new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest();
+        request.responseType = "json";
+
+        request.open('GET', 'https://private-anon-97425823a-trakt.apiary-mock.com/shows/trending');
+
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader('trakt-api-version', '2');
+        request.setRequestHeader('trakt-api-key', Ember.TRAKT_API_KEY);
+
+        request.onload = function() {
+          console.log(request.response);
+          resolve(request.response);
+        };
+
+        request.send();
+      }).then(function(shows) {
+        return Promise.all(shows.map(function(show) {
+          return new Promise(function(resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.responseType = "json";
+
+            request.open('GET', 'https://private-anon-97425823a-trakt.apiary-mock.com/search?id_type=trakt&id=' + show.show.ids.trakt);
+
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.setRequestHeader('trakt-api-version', '2');
+            request.setRequestHeader('trakt-api-key', 'b6ef9e6bad7815daa5d6df6cc236e3b0bf70606483209b404bfa7f7b31133884');
+
+            request.onload = function() {
+              console.log(request.response);
+              var details = request.response[0];
+              resolve(details.show || details.movie);
+            };
+
+            request.send();
+          });
+        })).then(function(results) {
+          _this.set('isSearching', false);
+          _this.set('results', results);
+          _this.set('resultCount', results.length);
+        });
+      });
+  },
+
+  actions: {
+      search: function(query) {
+          this.search(this.get('query'));
+      },
+
+      add: function(traktId, showTitle) {
+          if (window.confirm("Add " + showTitle + " to your list?")) {
+              // TODO: Add show
+          }
+      }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('series');
+  this.route('series-new');
 });
 
 export default Router;
