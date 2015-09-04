e11e66aff3716f644316b9714aff25e696859ddc
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-04 02:21:26 +0200

    Complete the previous fix

--- app/routes/episode.js
+++ app/routes/episode.js
@@ -4,7 +4,9 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.get('store').findRecord('series', params.showID).then(function(show) {
       return show.get("seasons").then(function(seasons) {
-        return seasons.toArray()[params.seasonNumber].get("episodes").then(function(episodes) {
+        return seasons.find(function(season) {
+          return season.get("number") == params.seasonNumber;
+        }).get("episodes").then(function(episodes) {
           return episodes.find(function(episode) {
             return episode.get("number") == params.episodeNumber;
           });
