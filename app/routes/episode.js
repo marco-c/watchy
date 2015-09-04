937c1ef8360c92b5d01a9a4ddd8f73763e396b2c
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-04 02:12:15 +0200

    Fix season and episode lookup

--- app/routes/episode.js
+++ app/routes/episode.js
@@ -5,7 +5,9 @@ export default Ember.Route.extend({
     return this.get('store').findRecord('series', params.showID).then(function(show) {
       return show.get("seasons").then(function(seasons) {
         return seasons.toArray()[params.seasonNumber].get("episodes").then(function(episodes) {
-          return episodes.toArray()[params.episodeNumber];
+          return episodes.find(function(episode) {
+            return episode.get("number") == params.episodeNumber;
+          });
         });
       });
     });
--- app/routes/season.js
+++ app/routes/season.js
@@ -4,7 +4,9 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.get('store').findRecord('series', params.showID).then(function(show) {
       return show.get("seasons").then(function(seasons) {
-        var season = seasons.toArray()[params.seasonNumber];
+        var season = seasons.find(function(season) {
+          return season.get("number") == params.seasonNumber;
+        });
         season.showID = params.showID;
         season.seasonNumber = params.seasonNumber;
         return season;
