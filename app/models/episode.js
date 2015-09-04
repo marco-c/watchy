ae2ec2b24040836dcaacf71f963b67daee624f78
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-04 02:03:56 +0200

    Add title and watched properties to the episode model

--- app/models/episode.js
+++ app/models/episode.js
@@ -2,4 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   number: DS.attr('number'),
+  title: DS.attr('string'),
+  watched: DS.attr('boolean', { defaultValue: false }),
 });
