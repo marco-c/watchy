1839f45f9fab1ff5997647df3400c6a36728255e
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-08 01:41:07 +0200

    Allow unmarking an episode

--- app/controllers/season.js
+++ app/controllers/season.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     mark: function(episode) {
-      episode.set("watched", true);
+      episode.set("watched", !episode.get("watched"));
       return episode.save();
     }
   }
