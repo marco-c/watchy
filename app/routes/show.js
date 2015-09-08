78d6bd6781fb440becaf6a2c6861162264aeb32c
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-08 13:11:28 +0200

    Add a progress bar (episodes seen/total episodes) for each season

--- app/routes/show.js
+++ app/routes/show.js
@@ -3,8 +3,29 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.get('store').findRecord('series', params.showID).then(function(show) {
-      show.showID = params.showID;
-      return show;
+      return show.get('seasons').then(function(seasons) {
+        var promises = [];
+
+        seasons.forEach(function(season) {
+          var episodesWatched = 0;
+
+          promises.push(season.get("episodes").then(function(episodes) {
+            episodes.forEach(function(episode) {
+              if (episode.get("watched")) {
+                episodesWatched++;
+              }
+            });
+
+            season.set('episodesNum', episodes.get("length"));
+            season.set('episodesWatched', episodesWatched);
+          }));
+        });
+
+        return Promise.all(promises).then(function() {
+          show.showID = params.showID;
+          return show;
+        });
+      });
     });
   }
 });
