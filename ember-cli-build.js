f7bb44a92c26606221e88a24fa759131bf0af636
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-08 04:13:00 +0200

    Import input_areas from the Gaia building blocks

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -23,19 +23,47 @@ module.exports = function(defaults) {
   app.import('bower_components/font-awesome/css/font-awesome.css');
   //app.import('bower_components/normalize-css/normalize.css');
 
+  app.import('vendor/gaia-2.0-bb/input_areas.css');
+  app.import('vendor/gaia-2.0-bb/input_areas/images/clear.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/clear_dark.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/dialog.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/dialog_active.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/dialog_disabled.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/dialog_disabled_rtl.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/dialog_rtl.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/search.svg', {
+    destDir: 'assets/input_areas/images'
+  });
+  app.import('vendor/gaia-2.0-bb/input_areas/images/search_dark.svg', {
+    destDir: 'assets/input_areas/images'
+  });
   app.import('vendor/gaia-2.0-bb/lists.css');
   app.import('vendor/gaia-2.0-bb/switches.css');
   app.import('vendor/gaia-2.0-bb/switches/images/check/default.png', {
-    destDir: "assets/switches/images/check"
+    destDir: 'assets/switches/images/check'
   });
   app.import('vendor/gaia-2.0-bb/switches/images/check/default@1.5x.png', {
-    destDir: "assets/switches/images/check"
+    destDir: 'assets/switches/images/check'
   });
   app.import('vendor/gaia-2.0-bb/switches/images/check/default@2.25x.png', {
-    destDir: "assets/switches/images/check"
+    destDir: 'assets/switches/images/check'
   });
   app.import('vendor/gaia-2.0-bb/switches/images/check/default@2x.png', {
-    destDir: "assets/switches/images/check"
+    destDir: 'assets/switches/images/check'
   });
 
   app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {
