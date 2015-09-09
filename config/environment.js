73027dc5102347d84ceae2d04a472cb1236d0178
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-09 21:41:22 +0200

    Set baseURL to watchy (for GitHub pages)

--- config/environment.js
+++ config/environment.js
@@ -40,6 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.baseURL = 'watchy/';
+
     ENV.serviceWorker = {
       enabled: true,
       serviceWorkerFile: "service-worker.js",
