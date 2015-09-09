240eded71b014848e6a99fc5351af4965cc834f8
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-09 20:28:49 +0200

    Add API key for the Watchy app (restricted to the GitHub page of this repo)

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ import config from './config/environment';
 
 var App;
 
-Ember.TRAKT_API_KEY = 'XXX';
+Ember.TRAKT_API_KEY = '7759eb155d9b5051c2b8c969cfb9a85cdc4b346bca5908e38c8b81a2b9e67387';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
