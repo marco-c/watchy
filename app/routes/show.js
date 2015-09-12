82236017e13383aebd97fc49ab23f851ee101b38
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-12 16:35:06 +0200

    Show special episodes as the last entry in the seasons list. Fixes #7"

--- app/routes/show.js
+++ app/routes/show.js
@@ -6,7 +6,26 @@ export default Ember.Route.extend({
       return show.get('seasons').then(function(seasons) {
         var promises = [];
 
+        // Sort by season number (with the special season at the end).
+        seasons = seasons.toArray().sort(function(a, b) {
+          if (a.get('number') === 0) {
+            return 1;
+          }
+
+          if (b.get('number') === 0) {
+            return -1;
+          }
+
+          return a.get('number') - b.get('number');
+        });
+
         seasons.forEach(function(season) {
+          if (season.get('number') === 0) {
+            season.computedName = 'Specials';
+          } else {
+            season.computedName = 'Season ' + season.get('number');
+          }
+
           var episodesWatched = 0;
 
           promises.push(season.get("episodes").then(function(episodes) {
@@ -23,6 +42,7 @@ export default Ember.Route.extend({
 
         return Promise.all(promises).then(function() {
           show.showID = params.showID;
+          show.computedSeasons = seasons;
           return show;
         });
       });
